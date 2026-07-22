337212a5b86d68944547f0e6aba1bcbca6b6d7a3
alesia <alesia>
2026-07-22 18:11:19 +0300

    done soap api

--- src/main/java/org/example/exceptions/DatatypeConfigurationException.java
+++
@@ -1,7 +0,0 @@
-package org.example.exceptions;
-
-public class DatatypeConfigurationException extends RuntimeException {
-    public DatatypeConfigurationException(String message) {
-        super(message);
-    }
-}
