14834d9e5f0b052821cd870e4ab300506565f093
alesia <alesia>
2026-08-12 17:40:23 +0300

    добавен WebSocket (кошмарики) для отправления результата проверки

--- src/main/java/org/example/kafka/ResultOfCheckng.java
+++ src/main/java/org/example/kafka/ResultOfCheckng.java
@@ -1,6 +1,8 @@
 package org.example.kafka;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.controllers.WebSocketHandler;
 import org.example.enums.TransactionStatus;
 import org.example.events.ResultOfChekingEvent;
 import org.example.exceptions.UnknownStatusException;
@@ -8,19 +10,34 @@ import org.example.utils.UpdateTracsationStatus;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ResultOfCheckng {
 
     private final UpdateTracsationStatus updateTracsationStatus;
+    private final WebSocketHandler webSocketHandler;
 
     @KafkaListener(topics = "result_of_checking")
-    public boolean resultOfChekingActivity(ResultOfChekingEvent event) {
+    public void resultOfChekingActivity(ResultOfChekingEvent event) {
+
+        String xml = """
+                <?xml version="1.0" encoding="UTF-8"?>
+                             <transactionResult xmlns="http://example.org/xsdPractice">
+                                 <number>%s</number>
+                                 <status>%s</status>
+                             </transactionResult>
+                """.formatted(event.transactionNumber(), event.status());
+
         if (event.status().equals(TransactionStatus.ACCEPTED)) {
             updateTracsationStatus.acceptedTransaction(event.transactionNumber());
+            webSocketHandler.sending(xml);
+
         } else if (event.status().equals(TransactionStatus.BLOCKED)) {
             updateTracsationStatus.blockedTransaction(event.transactionNumber());
+            webSocketHandler.sending(xml);
+        } else {
+            throw new UnknownStatusException("Неизвестный статус: " + event.status());
         }
-        throw new UnknownStatusException("Неизвестный статус: " + event.status());
     }
 }
